469aa4a9bbea1e408d46306b5ce39253464ec104
Robson Mendonça <rob.ams@gmail.com>
2023-03-20 13:37:04 -0300

    feat: iniciando repo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { ContactsService } from './services/contacts.services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portifolio.web';
+  name!: string;
+  email!: string;
+  subject!: string;
+  message!: string;
+
+  constructor( private contactService: ContactsService, private toastService: ToastrService){
+
+  }
+
+  sendMessage() {
+    this.contactService.addContact({
+      Name: this.name,
+      Email: this.email,
+      Subject: this.subject,
+      Message: this.message
+    }).subscribe(() => {
+      this.toastService.success('Mensagem enviada!', 'Sucesso')
+      this.name = ''
+      this.email = ''
+      this.subject = ''
+      this.message = ''
+    })
+  }
 }
---
+++ src/app/services/contacts.services.ts
@@ -0,0 +1,15 @@
+https://portifoloio-api-rob.azurewebsites.net/contactsimport { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import Contact from '../models/contacts';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactsService {
+  apiUrl = "https://portifoloio-api-rob.azurewebsites.net/contacts"
+  constructor(private http: HttpClient) { }
+
+  addContact(contact: Contact) {
+    return this.http.post(this.apiUrl, contact)
+  }
+}
